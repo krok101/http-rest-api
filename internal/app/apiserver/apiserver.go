16bd110bc5ffbfc4a9d78942202d1c367b6b2bfa
krok101g <krok101g@gmail.com>
2021-10-24 10:56:48 +0400

    start

---
+++ internal/app/apiserver/apiserver.go
@@ -0,0 +1,40 @@
+package apiserver
+
+import "github.com/sirupsen/logrus"
+
+// APIserver ...
+type APIserver struct {
+	config *Config
+	logger *logrus.Logger
+}
+
+// New ...
+func New(config *Config) *APIserver {
+	return &APIserver{
+		config: config,
+		logger: logrus.New(),
+	}
+}
+
+// Start ...
+func (s *APIserver) Start() error {
+	if err := s.configureLoger(); err != nil {
+		return err
+	}
+
+	s.logger.Info("starting api server ")
+
+	return nil
+}
+
+func (s *APIserver) configureLoger() error {
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+
+	if err != nil {
+		return err
+	}
+
+	s.logger.SetLevel(level)
+
+	return nil
+}
